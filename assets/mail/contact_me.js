8cf29bb5d717c032f297aef9fff5ca4ff343d423
Nonto <128770035+nontobeko1@users.noreply.github.com>
2024-04-30 14:57:05 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://r5hlyskcd4.execute-api.eu-west-1.amazonaws.com",
+                url: "https://pp86erquv0.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
