33e40b4e29b0e706987c6fcc366102120aeb25e4
Nonto <128770035+nontobeko1@users.noreply.github.com>
2024-05-04 01:02:53 +0200

    Update send_emails_with_ses.py

--- student_solution_files/send_emails_with_ses.py
+++ student_solution_files/send_emails_with_ses.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
     # Replace recipient@example.com with a "To" address. If your account 
     # is still in the sandbox, this address must be verified.
     # --- Insert your code here ---
-    RECIPIENT = 'edsa.predicts@explore-ai.ne' 
+    RECIPIENT = 'edsa.predicts@explore-ai.net' 
     # -----------------------------
 
 
